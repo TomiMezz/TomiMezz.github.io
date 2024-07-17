49181036ab30f4753009a228262279d44db009e4
TomiMezz <tmezzabarba@gmail.com>
2024-07-17 11:18:10 -0300

    update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,25 +97,26 @@
 	// Main Sections: Two.
 
 		// Lightbox gallery.
-			//$window.on('load', function() {
+		$window.on('load', function() {
 
-			//	$('#two').poptrox({
-			//		caption: function($a) { return $a.next('h3').text(); },
-			//		overlayColor: '#2c2c2c',
-			//		overlayOpacity: 0.85,
-			//		popupCloserText: '',
-			//		popupLoaderText: '',
-			//		selector: '.work-item a.image',
-			//		usePopupCaption: true,
-			//		usePopupDefaultStyling: false,
-			//		usePopupEasyClose: false,
-			//		usePopupNav: true,
-			//		windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-			//	});
+			$('#two').poptrox({
+				//caption: function($a) { return $a.next('h3').text(); },
+				caption: function($a) { return $a.attr('id'); },
+				overlayColor: '#2c2c2c',
+				overlayOpacity: 0.85,
+				popupCloserText: '',
+				popupLoaderText: '',
+				selector: '.work-item a.image',
+				usePopupCaption: true,
+				usePopupDefaultStyling: false,
+				usePopupEasyClose: false,
+				usePopupNav: true,
+				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+			});
 
-			//});
+		});
 	// Message:
-	
+			
 
 })(jQuery);
 
